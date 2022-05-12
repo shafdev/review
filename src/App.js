901fe845861cc7e3fe77bcd19e6f7f9839c00a2e
amrgitac <amrgitbackup@gmail.com>
2022-05-12 13:22:32 +0530

    c10

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Review from "./Review";
 function App() {
   return (
     <div className="container">
+    <div className="heading">
+      Reviews
+    </div>
+    <hr className="hrstyle"/>
       <Review/>
     </div>
   );
